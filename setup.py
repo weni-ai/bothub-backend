6429e5913b70f9dd72af3ff3530d7a68bc24323e
Daniel Yohan <dyohan9@gmail.com>
2019-11-07 17:20:51 -0300

    Updated Version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bothub_backend',
-    version='0.0.1',
+    version='1.0.2',
     description='Bothub NLP Backend',
     packages=find_packages(),
     install_requires=[
