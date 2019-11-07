ccec9ada80aac1f02620e42ab2a1ec07eec0e1e8
Daniel Yohan <dyohan9@gmail.com>
2019-11-07 17:20:02 -0300

    Added new router

--- bothub_backend/backend.py
+++ bothub_backend/backend.py
@@ -20,11 +20,15 @@ class BaseBackend(object):
         pass
 
     @abstractmethod
-    def request_backend_create_evaluate_results_intent(self, data, repository_authorization):
+    def request_backend_create_evaluate_results_intent(
+        self, data, repository_authorization
+    ):
         pass
 
     @abstractmethod
-    def request_backend_create_evaluate_results_score(self, data, repository_authorization):
+    def request_backend_create_evaluate_results_score(
+        self, data, repository_authorization
+    ):
         pass
 
     @abstractmethod
@@ -40,11 +44,23 @@ class BaseBackend(object):
         pass
 
     @abstractmethod
-    def request_backend_start_training_nlu(self, update_id, by, repository_authorization):
+    def request_backend_start_training_nlu(
+        self, update_id, by, repository_authorization
+    ):
         pass
 
     @abstractmethod
-    def request_backend_get_entities_and_labels_nlu(self, update_id, language, data, repository_authorization):
+    def request_backend_get_entities_and_labels_nlu(
+        self, update_id, language, data, repository_authorization
+    ):
+        pass
+
+    @abstractmethod
+    def request_backend_get_entities(self, update_id, repository_authorization):
+        pass
+
+    @abstractmethod
+    def request_backend_get_entities_labels(self, update_id, repository_authorization):
         pass
 
     @abstractmethod
@@ -52,7 +68,9 @@ class BaseBackend(object):
         pass
 
     @abstractmethod
-    def request_backend_traininglog_nlu(self, update_id, training_log, repository_authorization):
+    def request_backend_traininglog_nlu(
+        self, update_id, training_log, repository_authorization
+    ):
         pass
 
     @abstractmethod
@@ -60,9 +78,13 @@ class BaseBackend(object):
         pass
 
     @abstractmethod
-    def send_training_backend_nlu_persistor(self, update_id, botdata, repository_authorization):
+    def send_training_backend_nlu_persistor(
+        self, update_id, botdata, repository_authorization
+    ):
         pass
 
     @abstractmethod
-    def request_backend_repository_entity_nlu_parse(self, update_id, repository_authorization, entity):
+    def request_backend_repository_entity_nlu_parse(
+        self, update_id, repository_authorization, entity
+    ):
         pass
--- bothub_backend/bothub.py
+++ bothub_backend/bothub.py
@@ -10,163 +10,182 @@ class BothubBackend(BaseBackend):
 
     def request_backend_start_evaluation(self, update_id, repository_authorization):
         update = requests.get(
-            '{}/v2/repository/nlp/authorization/evaluate/evaluations/?update_id={}'.format(
-                self.backend,
-                update_id
+            "{}/v2/repository/nlp/authorization/evaluate/evaluations/?update_id={}".format(
+                self.backend, update_id
             ),
-            headers={'Authorization': 'Bearer {}'.format(repository_authorization)}
+            headers={"Authorization": "Bearer {}".format(repository_authorization)},
         ).json()
         return update
 
     def request_backend_create_evaluate_results(self, data, repository_authorization):
         update = requests.post(
-            '{}/v2/repository/nlp/authorization/evaluate/evaluate_results/'.format(
+            "{}/v2/repository/nlp/authorization/evaluate/evaluate_results/".format(
                 self.backend,
             ),
             data=data,
-            headers={'Authorization': 'Bearer {}'.format(repository_authorization)}
+            headers={"Authorization": "Bearer {}".format(repository_authorization)},
         ).json()
         return update
 
     def request_backend_create_evaluate_results_intent(
-        self,
-        data,
-        repository_authorization):
+        self, data, repository_authorization
+    ):
         update = requests.post(
-            '{}/v2/repository/nlp/authorization/evaluate/evaluate_results_intent/'.format(
+            "{}/v2/repository/nlp/authorization/evaluate/evaluate_results_intent/".format(
                 self.backend,
             ),
             data=data,
-            headers={'Authorization': 'Bearer {}'.format(repository_authorization)}
+            headers={"Authorization": "Bearer {}".format(repository_authorization)},
         ).json()
         return update
 
     def request_backend_create_evaluate_results_score(
-        self,
-        data,
-        repository_authorization):
+        self, data, repository_authorization
+    ):
         update = requests.post(
-            '{}/v2/repository/nlp/authorization/evaluate/evaluate_results_score/'.format(
+            "{}/v2/repository/nlp/authorization/evaluate/evaluate_results_score/".format(
                 self.backend,
             ),
             data=data,
-            headers={'Authorization': 'Bearer {}'.format(repository_authorization)}
+            headers={"Authorization": "Bearer {}".format(repository_authorization)},
         ).json()
         return update
 
     def get_langs(self):
         langs = requests.get(
-            '{}/v2/repository/nlp/authorization/langs/'.format(
-                self.backend,
-            )
+            "{}/v2/repository/nlp/authorization/langs/".format(self.backend,)
         ).json()
         return langs
 
     def request_backend_parse(self, router, repository_authorization, language=None):
         update = requests.get(
-            '{}/v2/repository/nlp/authorization/{}/{}/?language={}'.format(
-                self.backend,
-                router,
-                repository_authorization,
-                language
+            "{}/v2/repository/nlp/authorization/{}/{}/?language={}".format(
+                self.backend, router, repository_authorization, language
             ),
-            headers={'Authorization': 'Bearer {}'.format(repository_authorization)}
+            headers={"Authorization": "Bearer {}".format(repository_authorization)},
         ).json()
         return update
 
     def request_backend_parse_nlu(self, update_id, repository_authorization):
         update = requests.get(
-            '{}/v2/repository/nlp/update_interpreters/{}/'.format(
-                self.backend,
-                update_id
+            "{}/v2/repository/nlp/update_interpreters/{}/".format(
+                self.backend, update_id
             ),
-            headers={'Authorization': 'Bearer {}'.format(repository_authorization)}
+            headers={"Authorization": "Bearer {}".format(repository_authorization)},
         ).json()
         return update
 
-    def request_backend_start_training_nlu(self, update_id, by, repository_authorization):
+    def request_backend_start_training_nlu(
+        self, update_id, by, repository_authorization
+    ):
         update = requests.post(
-            '{}/v2/repository/nlp/authorization/train/start_training/'.format(
+            "{}/v2/repository/nlp/authorization/train/start_training/".format(
                 self.backend
             ),
-            data={
-                "update_id": update_id,
-                "by_user": by
-            },
-            headers={'Authorization': 'Bearer {}'.format(repository_authorization)}
+            data={"update_id": update_id, "by_user": by},
+            headers={"Authorization": "Bearer {}".format(repository_authorization)},
         ).json()
         return update
 
-    def request_backend_get_entities_and_labels_nlu(self, update_id, language, data, repository_authorization):
+    def request_backend_get_entities_and_labels_nlu(
+        self, update_id, language, data, repository_authorization
+    ):
         update = requests.get(
-            '{}/v2/repository/nlp/authorization/train/get_entities_and_labels/?update_id={}&language={}'.format(
-                self.backend,
-                update_id,
-                language
+            "{}/v2/repository/nlp/authorization/train/get_entities_and_labels/?update_id={}&language={}".format(
+                self.backend, update_id, language
             ),
             data=data,
             headers={
-                'Authorization': 'Bearer {}'.format(repository_authorization),
-                'Content-Type': 'application/json'
-            }
+                "Authorization": "Bearer {}".format(repository_authorization),
+                "Content-Type": "application/json",
+            },
         ).json()
+        return update
+
+    def request_backend_get_examples(
+        self, update_id, use_pagination=False, page=None, repository_authorization=None
+    ):
+        if not use_pagination:
+            update = requests.get(
+                "{}/v2/repository/nlp/authorization/train/get_examples/?update_id={}".format(
+                    self.backend, update_id
+                ),
+                headers={"Authorization": "Bearer {}".format(repository_authorization)},
+            ).json()
+        else:
+            update = requests.get(
+                page,
+                headers={"Authorization": "Bearer {}".format(repository_authorization)},
+            ).json()
+
+        return update
+
+    def request_backend_get_examples_labels(
+        self, update_id, use_pagination=False, page=None, repository_authorization=None
+    ):
+        if not use_pagination:
+            update = requests.get(
+                "{}/v2/repository/nlp/authorization/train/get_examples_labels/?update_id={}".format(
+                    self.backend, update_id
+                ),
+                headers={"Authorization": "Bearer {}".format(repository_authorization)},
+            ).json()
+        else:
+            update = requests.get(
+                page,
+                headers={"Authorization": "Bearer {}".format(repository_authorization)},
+            ).json()
         return update
 
     def request_backend_trainfail_nlu(self, update_id, repository_authorization):
         update = requests.post(
-            '{}/v2/repository/nlp/authorization/train/train_fail/'.format(
-                self.backend
-            ),
-            data={
-                'update_id': update_id
-            },
-            headers={'Authorization': 'Bearer {}'.format(repository_authorization)}
+            "{}/v2/repository/nlp/authorization/train/train_fail/".format(self.backend),
+            data={"update_id": update_id},
+            headers={"Authorization": "Bearer {}".format(repository_authorization)},
         ).json()
         return update
 
-    def request_backend_traininglog_nlu(self, update_id, training_log, repository_authorization):
+    def request_backend_traininglog_nlu(
+        self, update_id, training_log, repository_authorization
+    ):
         update = requests.post(
-            '{}/v2/repository/nlp/authorization/train/training_log/'.format(
+            "{}/v2/repository/nlp/authorization/train/training_log/".format(
                 self.backend
             ),
-            data={
-                'update_id': update_id,
-                'training_log': training_log
-            },
-            headers={'Authorization': 'Bearer {}'.format(repository_authorization)}
+            data={"update_id": update_id, "training_log": training_log},
+            headers={"Authorization": "Bearer {}".format(repository_authorization)},
         ).json()
         return update
 
     def request_backend_parse_nlu_persistor(self, update_id, repository_authorization):
         update = requests.get(
-            '{}/v2/repository/nlp/update_interpreters/{}/'.format(
-                self.backend,
-                update_id
+            "{}/v2/repository/nlp/update_interpreters/{}/".format(
+                self.backend, update_id
             ),
-            headers={'Authorization': 'Bearer {}'.format(repository_authorization)}
+            headers={"Authorization": "Bearer {}".format(repository_authorization)},
         ).json()
         return update
 
-    def send_training_backend_nlu_persistor(self, update_id, botdata, repository_authorization):
+    def send_training_backend_nlu_persistor(
+        self, update_id, botdata, repository_authorization
+    ):
         update = requests.post(
-            '{}/v2/repository/nlp/update_interpreters/'.format(
-                self.backend
-            ),
+            "{}/v2/repository/nlp/update_interpreters/".format(self.backend),
             data={
                 "id": update_id,
-                "bot_data": base64.b64encode(botdata).decode('utf8')
+                "bot_data": base64.b64encode(botdata).decode("utf8"),
             },
-            headers={'Authorization': 'Bearer {}'.format(repository_authorization)}
+            headers={"Authorization": "Bearer {}".format(repository_authorization)},
         ).json()
         return update
 
-    def request_backend_repository_entity_nlu_parse(self, update_id, repository_authorization, entity):
+    def request_backend_repository_entity_nlu_parse(
+        self, update_id, repository_authorization, entity
+    ):
         update = requests.get(
-            '{}/v2/repository/nlp/authorization/parse/repository_entity/?update_id={}&entity={}'.format(
-                self.backend,
-                update_id,
-                entity
+            "{}/v2/repository/nlp/authorization/parse/repository_entity/?update_id={}&entity={}".format(
+                self.backend, update_id, entity
             ),
-            headers={'Authorization': 'Bearer {}'.format(repository_authorization)}
+            headers={"Authorization": "Bearer {}".format(repository_authorization)},
         ).json()
         return update
