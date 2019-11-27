43289345c939360d69b13a36da1b8d1c9ad96462
Daniel Yohan <dyohan9@gmail.com>
2019-11-27 10:58:09 -0300

    Updated Router backend parse

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bothub_backend',
-    version='1.0.3',
+    version='1.0.4',
     description='Bothub NLP Backend',
     packages=find_packages(),
     install_requires=[
