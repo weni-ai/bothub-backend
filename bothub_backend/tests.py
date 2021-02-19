778704f69f3479a229e516391419a245022b1a8e
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2021-02-19 14:01:33 -0300

    add new agent to test nlp log

--- bothub_backend/tests.py
+++ bothub_backend/tests.py
@@ -406,3 +406,15 @@ class TestBothubBackend(unittest.TestCase):
         response = self.bh.send_log_nlp_parse(data=data)
 
         self.assertEqual(response, json)
+
+        data = {
+            "text": "test",
+            "from_backend": False,
+            "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) " + 
+                            "Chrome/87.0.4280.88 Safari/537.36",
+            "user": "teste"
+        }
+
+        response = self.bh.send_log_nlp_parse(data=data)
+
+        self.assertEqual(response, json)
