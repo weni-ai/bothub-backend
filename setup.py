d7b8efd57a9a12521c1802247c5cf342228c9e97
Lucas Agra <lucasomena@hotmail.com>
2021-09-14 14:50:27 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bothub_backend',
-    version='developt-1.0.23',
+    version='develop-1.0.23',
     description='Bothub NLP Backend',
     packages=find_packages(),
     install_requires=[
