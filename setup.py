e063d5d67796f70e7eab8997faece4655dcac83a
Lucas Agra de Omena <lucasomena@Hotmail.com>
2021-09-15 18:35:01 -0300

    Bump version 1.0.23

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bothub_backend',
-    version='develop-1.0.23',
+    version='1.0.23',
     description='Bothub NLP Backend',
     packages=find_packages(),
     install_requires=[
